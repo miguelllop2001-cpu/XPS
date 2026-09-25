9e6a861289dc129a0825fd18975150f672a5cb38
miguelllop2001-cpu <miguelllop2001@gmail.com>
2026-09-25 05:51:49 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Cada vez que subas una versión nueva, sube este número para que la actualización llegue.
-const CACHE = 'mi-negocio-v86';
+const CACHE = 'mi-negocio-v87';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
